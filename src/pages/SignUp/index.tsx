95af45002b94c1d94388529e7dea17f62e0e34a6
Leonardo <leo.deomedesse@voicce.com.br>
2020-07-14 18:49:15 -0300

    adicionada as paginas em rotas

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,6 +4,7 @@ import { FormHandles } from '@unform/core';
 // eslint-disable-next-line
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -59,10 +60,10 @@ const SignUp: React.FC = () => {
 
           <Button type="submit">Cadastrar</Button>
         </Form>
-        <a href="login">
+        <Link to="/">
           <FiArrowLeft />
           Voltar para logon
-        </a>
+        </Link>
       </Content>
     </Conteiner>
   );
